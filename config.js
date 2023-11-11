79f66eb60e7975a36e9e6ab29819f93aa936a149
tarek ali <108217205+6tarek@users.noreply.github.com>
2023-11-11 20:26:39 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bot: {
+   owners: ["1112033865645707314"],// owners id
+    clientSECRET: process.env.sec , // Client Secret
+    TOKEN: (process.env.token),
+    prefix : "&",
+    invite: "https://discord.com/api/oauth2/authorize?client_id=1166184355941580841&permissions=8&scope=bot",
+    category : "1166191078542680095",
+    transfering : "1112033865645707314",
+    price : "700",
+  },
+  website: {
+    PORT: "3001",//website port
+  }
+}
